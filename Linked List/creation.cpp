c7cd28e96b8dd494eec1bbb65e8eaa2f92ff9735
rv.git <rv.cse@outlook.com>
2025-09-29 21:23:12 +0530

    Linked List

---
+++ Linked List/creation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    struct Node{
+
+    };
+
+    return 0;
+}
---
+++ Linked List/syntax.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    struct Node{
+        int data;      // Data Part
+        Node* next;    // Pointer to the Next
+    };
+
+    return 0;
+}
