443f4a750efb4f788b9cf4e53c1a99e20149f085
rv.git <rv.cse@outlook.com>
2025-10-05 23:52:59 +0530

    Changes

--- Linked List/creation.cpp
+++ Linked List/creation.cpp
@@ -1,10 +1,33 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    struct Node{
+struct Node{
+    int data;
+    Node* next; 
+};
 
-    };
+int main(){
+    Node* first = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    first->data = 15 ;
+    second->data = 20 ;
+    third->data = 30 ;
+
+
+    first->next = second ;
+    second->next = third;
+    third->next = NULL ; 
+
+    Node*temp = first;
+    while(temp != NULL){
+        cout << temp->data << " " ;
+        temp = temp->next;
+
+    }
+
+    cout << "NULL" << "\n" ;
 
     return 0;
 }
